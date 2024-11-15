9c610ca748b79aa8036528ef269a29ce392bea4d
miguelLV <miguel.letelier@ug.uchile.cl>
2024-11-14 22:11:32 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -887,7 +887,7 @@ class dislon_lattice:
         for k in range(len(self.kpath)):
           dispersion[k] = self.phonon_eigensystem[k].eigenenergies
         for i in range(3):
-            plt.plot(dispersion[:,])
+            plt.plot(dispersion[:,i])
         print(dispersion)
         plt.show()
     
