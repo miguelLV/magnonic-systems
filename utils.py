ec02bbb5f6fc7563bc4d36003b4892256ede3471
miguelLV <miguel.letelier@ug.uchile.cl>
2025-04-02 00:35:22 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -444,7 +444,7 @@ class lattice:
                 if isneighbor:
                     actual_bond = r_i+self.displacement(r_i, c)-(r_j+self.displacement(r_j, c))
                     actual_bond_size = np.linalg.norm(actual_bond)
-                    J_ij = -J*(1-magnetoelastic_coupling*(actual_bond_size/self.lattice_constant-1))
+                    J_ij = J*(1-magnetoelastic_coupling*(actual_bond_size/self.lattice_constant-1))
                     Hamiltonian[i,j] = J_ij
                     Hamiltonian[j,i] = np.conj(J_ij)
         return Hamiltonian*S
